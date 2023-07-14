70c696376d529f40cb1939d559ad8d2075bbb0da
nbcarroll <nickbcarroll@gmail.com>
2023-07-14 12:11:13 -0700

    Update request.py

--- flask_test/request.py
+++ flask_test/request.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://nbcarroll.github.io/flask_test/results'
+url = 'https://nbcarroll.github.io/flask_test/'
 r = requests.post(url,json={'rate':5, 'sales_in_first_month':200, 'sales_in_second_month':400})
 
 print(r.json())
